018a7e558e334715fe415f61eb7288f3fa1b708c
robertriosm <87707371+robertriosm@users.noreply.github.com>
2022-04-26 10:08:18 -0600

    bugs de babel+webpack reparados
    se agregaron loaders de html

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import react from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>HELLO world</h1>
+    </div>
+  );
+}
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>hello world!</h1>
+//     </div>
+//   );
+// };
+
+export default App;
